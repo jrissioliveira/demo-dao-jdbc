46468201848087335d2d9ecc1b6283c022c1afed
Isaias Jair de Oliveira Junior <jrissi2008@gmail.com>
2019-07-02 17:37:39 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import modal.dao.DaoFactory;
+import modal.dao.SellerDao;
 import modal.entities.Department;
 import modal.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail", new Date(), 3000.00, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 		
---
+++ src/modal/dao/DaoFactory.java
@@ -0,0 +1,11 @@
+package modal.dao;
+
+import modal.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+	
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+	
+}
