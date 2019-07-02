b743aa3e2f3f553757c53c39b9c92c52ecae6657
Isaias Jair de Oliveira Junior <jrissi2008@gmail.com>
2019-07-02 17:11:09 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import modal.entities.Department;
+import modal.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail", new Date(), 3000.00, obj);
+		
+		System.out.println(seller);
 		
 	}
 
